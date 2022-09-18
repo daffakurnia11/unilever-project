6d48b08c6aea97946286d9c0f9626ad41152dd4f
Daffa Kurnia Fatah <72376750+daffakurnia11@users.noreply.github.com>
2022-09-19 06:56:22 +0700

    Syncronize user logged in

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
 
   <!--Header-->
   <div class="page-breadcrumb d-flex flex-column flex-md-row align-items-center mb-3">
-    <div class="breadcrumb-title pe-md-3">Hallo, Admin</div>
+    <div class="breadcrumb-title pe-md-3">Hallo, {{ auth()->user()->name }}</div>
     <div class="ps-md-3 ms-md-auto mx-auto mx-md-0 mt-3 mt-md-0">
       <nav aria-label="breadcrumb">
         <ol class="breadcrumb mb-0 p-0">
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -24,7 +24,7 @@
               <a class="nav-link dropdown-toggle dropdown-toggle-nocaret" href="#" data-bs-toggle="dropdown">
                 <div class="user-setting d-flex align-items-center gap-1">
                   <img src="/img/avatar-1.png" class="user-img" alt="">
-                  <div class="user-name d-none d-sm-block">Admin</div>
+                  <div class="user-name d-none d-sm-block">{{ auth()->user()->name }}</div>
                 </div>
               </a>
               <ul class="dropdown-menu dropdown-menu-end">
@@ -33,15 +33,15 @@
                     <div class="d-flex align-items-center">
                       <img src="/img/avatar-1.png" alt="" class="rounded-circle" width="60" height="60">
                       <div class="ms-3">
-                        <h6 class="mb-0 dropdown-user-name">Admin</h6>
-                        <small class="mb-0 dropdown-user-designation text-secondary">Admin</small>
+                        <h6 class="mb-0 dropdown-user-name">{{ auth()->user()->name }}</h6>
+                        {{-- <small class="mb-0 dropdown-user-designation text-secondary">Admin</small> --}}
                       </div>
                     </div>
                   </a>
                 </li>
                 <li><hr class="dropdown-divider"></li>
                 <li>
-                  <a class="dropdown-item" href="pages-user-profile.html">
+                  <a class="dropdown-item" href="/">
                       <div class="d-flex align-items-center">
                         <div class="setting-icon"><i class="bi bi-person-fill"></i></div>
                         <div class="setting-text ms-3"><span>Profile</span></div>
@@ -50,12 +50,15 @@
                 </li>
                 <li><hr class="dropdown-divider"></li>
                 <li>
-                  <a class="dropdown-item" href="authentication-signup-with-header-footer.html">
+                  <form action="/logout" method="POST">
+                    @csrf
+                    <button type="submit" class="dropdown-item">
                       <div class="d-flex align-items-center">
                         <div class="setting-icon"><i class="bi bi-lock-fill"></i></div>
                         <div class="setting-text ms-3"><span>Logout</span></div>
                       </div>
-                    </a>
+                    </button>
+                  </form>
                 </li>
               </ul>
             </li>
